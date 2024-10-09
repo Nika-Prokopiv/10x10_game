ed826c45cd1c8c1be1cf6cd714b790527d95654e
Nika-Prokopiv <prokopivvu@gmail.com>
2024-10-09 23:40:12 +0300

    create play-field component

---
+++ src/app/play-field/play-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-play-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './play-field.component.html',
+  styleUrl: './play-field.component.css'
+})
+export class PlayFieldComponent {
+
+}
