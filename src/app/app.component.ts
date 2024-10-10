e2bfbaf2453d2eaf3e58a604cf7350626530816b
Nika-Prokopiv <prokopivvu@gmail.com>
2024-10-10 18:27:44 +0300

    add modal window layout & styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import {ModalWindowComponent} from "./modal-window/modal-window.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent],
+  imports: [RouterOutlet, LayoutComponent, ModalWindowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/modal-window/modal-window.component.ts
+++ src/app/modal-window/modal-window.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-modal-window',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './modal-window.component.html',
   styleUrl: './modal-window.component.css'
 })
 export class ModalWindowComponent {
+  isOpen: boolean = true;
 
+  closeModal() {
+    this.isOpen = false;
+  }
+
+  getGameResult() {
+    return 'Game result';
+  }
 }
